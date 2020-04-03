e6beb0ddf6874f277a90c2cbc1309c1232ea0b65
Kim Astrom <kim.astrom@gmail.com>
2020-04-03 19:06:00 +0200

    header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React from 'react';
+import Header from '../components/header/index';
 
 export default function IndexPage() {
   return (
     <div>
+      <Header />
       <h1>Index</h1>
     </div>
   );
