edbaca89e697d5d81b07457df00190b1458fbcd1
Kim Astrom <kim.astrom@gmail.com>
2020-04-04 22:34:28 +0200

    styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ import Search from '../search';
 const Header = () => {
   return (
     <div className="Header">
-      <div className="Header__container">
+      <div className="Header__container container">
         <p className="Header__container__tagline">
           Let's fight the virus by reducing crowding in our society
         </p>
