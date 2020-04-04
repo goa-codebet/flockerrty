b9fdb43b9f941848ee60f5d89fcf3324ac81a258
Kim Astrom <kim.astrom@gmail.com>
2020-04-04 15:55:16 +0200

    add header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-import './header.scss';
+import './style.scss';
+import Search from '../search';
 
 export default function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="header__container">
+        <p className="header__container__tagline">
+          Let's fight the virus by reducing crowding in our society
+        </p>
+        <Search />
+      </div>
+    </div>
+  );
 }
