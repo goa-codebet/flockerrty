07e629a62ee0151b068b83528307c6af41106da7
Kim Astrom <kim.astrom@gmail.com>
2020-04-05 18:40:32 +0200

    Add default search results

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import PlacesView from '../components/places-list-view';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 const SearchPage = () => {
   const { q } = useParams();
+
+  const NearbyPlacesView = props => {
+    const { loading, data, error } = useQuery(gql`
+      {
+        nearby(
+          location: "56.6702937,16.2976897"
+          radius: 50000
+          category: "supermarkets"
+        ) {
+          name
+          city
+          photo
+          heatmap {
+            time
+            value
+          }
+          place_id
+        }
+      }
+    `);
+    return (
+      <PlacesView
+        {...props}
+        loading={loading}
+        error={error}
+        data={data ? data.nearby : []}
+      />
+    );
+  };
+
   return (
     <div>
-      <div className="container">
-        <h1>Search</h1>
-        <p>q: {q}</p>
-      </div>
+      <NearbyPlacesView />
     </div>
   );
 };
